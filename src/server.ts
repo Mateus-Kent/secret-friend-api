fe616bb6e45df562d3474bcef26fb424a4bdfb7a
mateus-kent <mateusquent2003@gmail.com>
2025-01-21 15:22:52 -0300

    feat: add function to crate group and add participants

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,34 @@
 import Fastify from "fastify";
+import prisma from "./prisma";
 
 const server = Fastify();
 
 server.get("/", async (request, reply) => {
   return { message: "Hello, Fastify with TypeScript!" };
+});
+
+server.post("/create-group", async (request, reply) => {
+  const { name, participants } = request.body as {
+    name: string;
+    participants: { name: string; email: string }[];
+  };
+
+  try {
+    const group = await prisma.group.create({
+      data: {
+        name,
+        participants: {
+          create: participants,
+        },
+      },
+      include: { participants: true },
+    });
+
+    return reply.status(201).send(group);
+  } catch (error) {
+    console.error(error);
+    return reply.status(500).send({ error: "Failed to create group" });
+  }
 });
 
 const start = async () => {
